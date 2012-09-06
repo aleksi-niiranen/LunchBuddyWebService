689fa612dc7a7779ffa566966857fa490ddab1cb
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2012-09-06 18:15:18 +0300

    date test

--- grails-app/controllers/com/blogspot/fwfaill/lunchbuddywebservice/ScrapeController.groovy
+++ grails-app/controllers/com/blogspot/fwfaill/lunchbuddywebservice/ScrapeController.groovy
@@ -3,16 +3,21 @@ package com.blogspot.fwfaill.lunchbuddywebservice
 class ScrapeController {
 
     def scrape = {
-        def menu = Scraper.scrape()
+        //def menu = Scraper.scrape()
 
-        for(int i = 0; i < 5; i++) {
-            def newMenu = new Menu(timestamp:50000).save(flush:true)
-            menu[Scraper.WEEKDAYS[i]].each {
-                def course = new Course(titleFi:it.title_fi, titleEn:it.title_en, limitations:it.properties, price:it.price, menu:newMenu).save(flush:true)
-            }
-        }
+        //for(int i = 0; i < 5; i++) {
+        //    def newMenu = new Menu(timestamp:50000).save(flush:true)
+        //    menu[Scraper.WEEKDAYS[i]].each {
+        //        def course = new Course(titleFi:it.title_fi, titleEn:it.title_en, limitations:it.properties, price:it.price, menu:newMenu).save(flush:true)
+        //    }
+        //}
 
-        def c = new GregorianCalendar()
-        render "time: ${c.time}"
+        def c = new GregorianCalendar(TimeZone.getTimeZone("Europe/Helsinki"), new Locale("Finnish", "Finland"))
+        c.set(Calendar.HOUR_OF_DAY, 0)
+        c.set(Calendar.MINUTE, 0)
+        c.set(Calendar.SECOND, 0)
+        c.set(Calendar.MILLISECOND, 0)
+
+        render "time: ${c.time}\n timeInSeconds: ${c.timeInMillis / 1000}"
     }
 }
