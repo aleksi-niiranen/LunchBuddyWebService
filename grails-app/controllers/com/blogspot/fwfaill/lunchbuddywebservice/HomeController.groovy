e73f9ebb3278c3e34ad7923b4ace96ada355f65c
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2012-10-19 12:36:29 +0300

    heroku testing

--- grails-app/controllers/com/blogspot/fwfaill/lunchbuddywebservice/HomeController.groovy
+++ grails-app/controllers/com/blogspot/fwfaill/lunchbuddywebservice/HomeController.groovy
@@ -3,4 +3,10 @@ package com.blogspot.fwfaill.lunchbuddywebservice
 class HomeController {
 
     def index() { }
+
+    def countCourses() {
+        def coursesCount = Course.list().size()
+
+        render "courses list size: ${coursesCount}"
+    }
 }
